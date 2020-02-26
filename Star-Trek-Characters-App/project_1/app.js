e93539a9d7718d64deb64d30587474f5eb2c60b7
Maddy Lewald <madeline_irene22@hotmail.com>
2020-02-25 21:13:20 -0500

    fixing ajax error

--- Star-Trek-Characters-App/project_1/app.js
+++ Star-Trek-Characters-App/project_1/app.js
@@ -7,7 +7,7 @@ $(() => {
     const userInput = $('input[type="text"]').val();
     //lets you put stuff in the form
   $.ajax ({
-    url: 'http://stapi.co/api/v1/rest/character/search',
+    url: 'https://stapi.co/api/v1/rest/character/search',
   }).then (
 //the url that characters are grabbed from
     (data) => {
